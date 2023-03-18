c66b43e8fda9f9044a10bcdbaba2387e2b4e6768
surya9393 <surya.065118107@unpak.ac.id>
2023-03-18 17:18:43 +0700

    add link Linkedin, Github, Instagram

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,9 +10,9 @@
                         <p class="card-text">Dibuat oleh:</p>
                         <p class="card-text">Surya Adi Saputro</p>
                         <div class="d-flex justify-content-center gap-3">
-                            <a href="/login" class="btn btn-dark">Github</a>
-                            <a href="/login" class="btn btn-primary">Linkedin</a>
-                            <a href="/login" class="btn btn-danger">Instagram</a>
+                            <a href="https://github.com/surya9393/" class="btn btn-dark">Github</a>
+                            <a href="https://www.linkedin.com/in/surya-adi-saputro-248695209/" class="btn btn-primary">Linkedin</a>
+                            <a href="https://www.instagram.com/srydsprt93/" class="btn btn-danger">Instagram</a>
                         </div>
                     </div>
                 </div>
